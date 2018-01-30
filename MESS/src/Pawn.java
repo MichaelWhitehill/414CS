e5141a3d327474092d3311a4811626de0c3ced03
Michael <michaellwhitehill@gmail.com>
2018-01-29 23:02:31 -0700

    Pawn class (#6)
    Pawns are so simple, but so stupid difficult.

--- MESS/src/Pawn.java
+++ MESS/src/Pawn.java
@@ -1,3 +1,4 @@
+import java.rmi.activation.ActivationGroup_Stub;
 import java.util.ArrayList;
 
 public class Pawn extends ChessPiece {
@@ -10,7 +11,45 @@ public class Pawn extends ChessPiece {
 
     @Override
     public ArrayList<String> legalMoves() {
-        return null;
+        ArrayList<String> moves = new ArrayList<>();
+        if (color == Color.BLACK) {
+            addMove(row - 1, column, moves);
+            if (row == 6)
+                addMove(row - 2, column, moves);
+        }
+        if (color == Color.WHITE) {
+            addMove(row + 1, column, moves);
+            if (row == 1)
+                addMove(row + 2, column, moves);
+        }
+        checkDiagonal(moves);
+        return moves;
+    }
+
+    private void checkDiagonal(ArrayList<String> moves){
+        try{
+            if (color == Color.BLACK){
+                // down left
+                if(isInbounds(row-1, column - 1) && board.getPiece(ChessBoard.toChessCoordinate(row-1, column-1))!=null)
+                    if (board.getPiece(ChessBoard.toChessCoordinate(row-1, column-1)).color != color)
+                        moves.add(ChessBoard.toChessCoordinate(row-1, column-1));
+                // down right
+                if(isInbounds(row-1, column + 1) && board.getPiece(ChessBoard.toChessCoordinate(row-1, column+1))!=null)
+                    if (board.getPiece(ChessBoard.toChessCoordinate(row-1, column+1)).color != color)
+                        moves.add(ChessBoard.toChessCoordinate(row-1, column+1));
+            } else {
+                // up left
+                if(isInbounds(row+1, column - 1) && board.getPiece(ChessBoard.toChessCoordinate(row+1, column-1))!=null)
+                    if (board.getPiece(ChessBoard.toChessCoordinate(row+1, column-1)).color != color)
+                        moves.add(ChessBoard.toChessCoordinate(row+1, column-1));
+                // up right
+                if(isInbounds(row+1, column + 1) && board.getPiece(ChessBoard.toChessCoordinate(row+1, column+1))!=null)
+                    if (board.getPiece(ChessBoard.toChessCoordinate(row+1, column+1)).color != color)
+                        moves.add(ChessBoard.toChessCoordinate(row+1, column+1));
+            }
+        } catch (IllegalPositionException e){
+            e.printStackTrace();
+        }
     }
 
     public boolean equals(Object other){
